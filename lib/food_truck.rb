d1957dcf3e74bebd1016176cab1d5242e64ecb3c
Kevin Cuadros <kev040798@gmail.com>
2020-09-15 17:09:44 -0400

    Iterartion 1

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -2,21 +2,14 @@ class FoodTruck
   attr_reader :name, :inventory
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
   end
 
   def check_stock(item)
-    if @inventory.key? item
-      x = @inventory.find do |name, price|
-        name == item
-      end[1]
-    else
-      0
-    end
+    @inventory[item]
   end
 
   def stock(item, amount)
-    x = 0
-    @inventory[item] = (x += amount)
+    @inventory[item] += amount
   end
 end
--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -20,8 +20,9 @@ class FoodTruckTest < Minitest::Test
   def test_food_truck_has_stock
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
-    assert_equal 0, food_truck.stock(item1)
+    assert_equal 0, food_truck.stock(item1, 0)
 
     food_truck.stock(item1, 30)
 
@@ -33,7 +34,6 @@ class FoodTruckTest < Minitest::Test
     food_truck.stock(item1, 25)
 
     assert_equal 55, food_truck.check_stock(item1)
-    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
     food_truck.stock(item2, 12)
 
